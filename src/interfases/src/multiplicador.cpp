837ab7c6fd3d65ece1925c60e5640486768d3f13
“casta42” <gerardocastanon42@gmail.com>
2023-05-29 00:12:47 -0600

    hpp description added

--- src/interfases/src/multiplicador.cpp
+++ src/interfases/src/multiplicador.cpp
@@ -1,6 +1,6 @@
 #include "multiplicador.hpp"
 #include "stdio.h"
-
+/*! funcion que multiplica valores de la coordenada x100*/
 int multiplicar_por_100(int x){
     int res = x*100;
     return res;
