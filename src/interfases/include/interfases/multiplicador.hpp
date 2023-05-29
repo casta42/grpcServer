c07777fe5a62153c482cddec0bff824b2de5accc
“casta42” <gerardocastanon42@gmail.com>
2023-05-29 00:15:00 -0600

    descriptions added

--- src/interfases/include/interfases/multiplicador.hpp
+++ src/interfases/include/interfases/multiplicador.hpp
@@ -1,6 +1,6 @@
 #ifndef MULTIPLICADOR_HPP
 #define MULTIPLICADOR_HPP
-
+/*! declaracion de la funcion que multiplica valores de la coordenada x100*/
 extern "C" {
     int multiplicar_por_100(int x);
 }
